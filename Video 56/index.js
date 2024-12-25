b17249e3e43cc42337c454f849a212928fcacae9
Divyam <divyamyadav40@gmail.com>
2024-12-25 22:42:11 +0530

    VIDEO 56

---
+++ Video 56/index.js
@@ -0,0 +1,19 @@
+console.log('Conditionals in javascript')
+
+// a = 2
+// b = 4
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a**b)
+// console.log(a%b)
+
+age = 18
+
+if(age==18){
+    console.log('You can drive')
+} else {
+    console.log('You cannot drive')
+}
